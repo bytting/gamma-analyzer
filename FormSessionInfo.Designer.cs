e49d086d4633ecd7a33b69cc4fae8e57516325f5
Dag Robøle <dag.robole@nrpa.no>
2016-04-27 15:52:31 +0200

    Added session info files

---
+++ FormSessionInfo.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace crash
+{
+    partial class FormSessionInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormSessionInfo));
+            this.tools = new System.Windows.Forms.ToolStrip();
+            this.status = new System.Windows.Forms.StatusStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tools
+            // 
+            this.tools.Location = new System.Drawing.Point(0, 0);
+            this.tools.Name = "tools";
+            this.tools.Size = new System.Drawing.Size(670, 25);
+            this.tools.TabIndex = 0;
+            this.tools.Text = "toolStrip1";
+            // 
+            // status
+            // 
+            this.status.Location = new System.Drawing.Point(0, 402);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(670, 22);
+            this.status.TabIndex = 1;
+            this.status.Text = "statusStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.btnOk);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 373);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(670, 29);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnOk.Location = new System.Drawing.Point(595, 0);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 29);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnCancel.Location = new System.Drawing.Point(520, 0);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 29);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // FormSessionInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 424);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.tools);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormSessionInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Crash - Session Info";
+            this.Load += new System.EventHandler(this.FormSessionInfo_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip tools;
+        private System.Windows.Forms.StatusStrip status;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
---
+++ FormSessionInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace crash
+{
+    public partial class FormSessionInfo : Form
+    {
+        private Session session = null;
+
+        public FormSessionInfo(Session sess, string title)
+        {
+            InitializeComponent();
+            Text = "Crash - " + title;
+            session = sess;
+        }
+
+        private void FormSessionInfo_Load(object sender, EventArgs e)
+        {            
+            //
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {            
+            Close();
+        }        
+    }
+}
