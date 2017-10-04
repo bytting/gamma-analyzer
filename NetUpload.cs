4f077b88193249b85d3ee0ac42451f49efd07cf0
Dag Robøle <dag.robole@nrpa.no>
2017-10-04 14:49:02 +0200

    Added timeout for web requests

--- NetUpload.cs
+++ NetUpload.cs
@@ -41,39 +41,55 @@ namespace crash
                         if (String.IsNullOrEmpty(hostname))
                             continue;
 
-                        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://" + hostname + "/add-spectrum");
-                        request.Method = WebRequestMethods.Http.Post;
-                        request.Accept = "application/json";
+                        HttpWebRequest request = null;
 
-                        SimpleSpectrum simpleSpec = new SimpleSpectrum(spec);
-                        Log.Info("Sending " + simpleSpec.SessionName + ":" + simpleSpec.SessionIndex);
-
-                        var jsonRequest = JsonConvert.SerializeObject(simpleSpec);
-                        var data = Encoding.UTF8.GetBytes(jsonRequest);
-
-                        request.Method = "POST";
-                        request.ContentType = "application/json";
-                        request.ContentLength = data.Length;
-
-                        using (var stream = request.GetRequestStream())
+                        try
                         {
-                            stream.Write(data, 0, data.Length);
-                        }
+                            request = (HttpWebRequest)WebRequest.Create("http://" + hostname + "/add-spectrum");
+                            request.Timeout = 2000;
+                            request.Method = WebRequestMethods.Http.Post;
+                            request.Accept = "application/json";
 
-                        string jsonText;
-                        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                        if (response == null || response.StatusCode != HttpStatusCode.OK)
+                            SimpleSpectrum simpleSpec = new SimpleSpectrum(spec);
+                            Log.Info("Sending " + simpleSpec.SessionName + ":" + simpleSpec.SessionIndex);
+
+                            var jsonRequest = JsonConvert.SerializeObject(simpleSpec);
+                            var data = Encoding.UTF8.GetBytes(jsonRequest);
+
+                            request.Method = "POST";
+                            request.ContentType = "application/json";
+                            request.ContentLength = data.Length;
+
+                            using (var stream = request.GetRequestStream())
+                            {
+                                stream.Write(data, 0, data.Length);
+                            }
+
+                            string jsonText;
+                            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                            if (response == null || response.StatusCode != HttpStatusCode.OK)
+                            {
+                                Log.Error("Upload failed (" + response.StatusCode + ")");
+                                continue;
+                            }
+
+                            using (var sr = new StreamReader(response.GetResponseStream()))
+                            {
+                                jsonText = sr.ReadToEnd();
+                            }
+
+                            Log.Info(jsonText);
+                        }
+                        catch(WebException ex)
                         {
-                            Log.Error("Upload failed (" + response.StatusCode + ")");
-                            continue;
-                        }
-
-                        using (var sr = new StreamReader(response.GetResponseStream()))
-                        {
-                            jsonText = sr.ReadToEnd();
-                        }
-
-                        Log.Info(jsonText);
+                            if(ex.Status == WebExceptionStatus.Timeout)
+                            {
+                                Log.Error("Web service timeout");
+                                request.Abort();
+                                continue;
+                            }
+                            Log.Error("Exception", ex);
+                        }                        
                     }
                 }
 
