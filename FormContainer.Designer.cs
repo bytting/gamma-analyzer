f1f3a7d45430fc0e9c274f74b3cfc10f75ee29c5
Dag Robøle <dag.robole@nrpa.no>
2017-10-19 19:03:03 +0200

    Added runtime dependencies to setup

--- FormContainer.Designer.cs
+++ FormContainer.Designer.cs
@@ -296,6 +296,7 @@
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(1159, 634);
             this.Controls.Add(this.status);
             this.Controls.Add(this.tools);
